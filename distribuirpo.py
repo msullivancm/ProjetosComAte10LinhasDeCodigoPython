e53a96ced98f5c32d60f6760c7f1afacd1137f3d
msullivancm <marcus.sullivan@gmail.com>
2023-03-21 12:17:41 -0300

    distribui rpo corrigido e gravando um pasta só de rpo e não uma por appserver como antes

--- distribuirpo.py
+++ distribuirpo.py
@@ -7,13 +7,13 @@ from tqdm import tqdm
 from six.moves import configparser as ConfigParser
 """ Para gerar o executável devemos:
 pip3 install pyinstaller
-python -m PyInstaller distribuirpo.py"""
+python -m PyInstaller --onefile distribuirpo.py"""
 
 #atribuiçaõ de variáveis
 arqConfigExist=False
 try:
-    f = open('distribuirpo.ini')
-    f.close()
+    VerificaIni = open('distribuirpo.ini')
+    VerificaIni.close()
     arqConfigExist=True
     cfg = ConfigParser.ConfigParser()
     cfg.read('distribuirpo.ini')
@@ -25,7 +25,9 @@ if arqConfigExist:
     dirDestApo = cfg.get('destino', 'dirDestApo')
     dirInis = cfg.get('arquivos','dirInis')
     arqIni = cfg.get('arquivos','arqIni')
-    arqApo = cfg.get('arquivos','arqApo')
+    aArqApo = cfg.get('arquivos','aArqApo').split(',')
+    aChave = cfg.get('arquivos','aChave').split(',')
+    aDirIni = cfg.get('destino','aDirIni').split(',')
 else:
     dirOrigApo = input("Informe o diretório origem do APO(Se não for informado o padrão será utilizada): ")
     if dirOrigApo:
@@ -50,18 +52,31 @@ else:
     else:
         arqIni = 'appserver.ini'
 
-    arqApo = input("Informe o nome do arquivo APO de origem que será copiado para o destino(Se não for informado o padrão será utilizada): ")
-    if arqApo:
+    aArqApo = input("Informe o nome do arquivo APO de origem que será copiado para o destino(Se não for informado o padrão será utilizada): ")
+    if aArqApo:
         pass
     else:
-        arqApo = 'tttm120.rpo'
+        aArqApo = ['tttm120.rpo']
 
+    aChave = input("Informe o nome da chave que deverá ser alterada(Se não for informado o padrão será utilizada): ")
+    if aChave:
+        pass
+    else:
+        aChave = ['sourcepath']
+
+    aDirIni = input("Informe o nome do diretório onde estão os INIs(Se não for informado o padrão será utilizada): ")
+    if aDirIni:
+        pass
+    else:
+        aDirIni = 'appserver'
+        
 #Lê o diretório onde tem os INIs e guarda
 def leInis(dirInis):
     aInis=[]
     os.chdir(dirInis)
     for d in tqdm(os.listdir()):
-        aInis.append(os.path.abspath(d))
+        if d in aDirIni: 
+            aInis.append(os.path.abspath(d))
     return aInis
 
 #Lê o diretório de destino dos APOs e guarda
@@ -75,38 +90,63 @@ def leDirApos(dirDestApo):
 #Gera diretório com a data e hora atuais em cada uma das pastas de destino do Apo
 def geraDirDataHoraDestAPOs(aDirApos):
     aDirDataHoraDestApos = []
-    for d in aDirApos:
-        dirDataHora = datetime.now().strftime('%Y%m%d%H%M')
-        aDirDataHoraDestApos.append(f'{d}\{dirDataHora}')
-    return aDirDataHoraDestApos
+    dirDataHora = datetime.now().strftime('%Y%m%d%H%M')
+    dirDataHoraDestApos=f'{dirDestApo}\{dirDataHora}'
+    return dirDataHoraDestApos
 
 #Cria diretório com a data e hora atuais em cada uma das pastas de destino do Apo
-def criaDirDataHoraDestAPOs(aDirDataHoraDestApos):
-    for d in aDirDataHoraDestApos:
-        print(f'Criando o diretório: {d}')
-        try:
-            os.mkdir(d, 777)
-        except OSError as err:
-            return print(err)
+def criaDirDataHoraDestAPOs(dirDataHoraDestApos):
+    print(f'Criando o diretório: {dirDataHoraDestApos}')
+    try:
+        os.mkdir(dirDataHoraDestApos, 777)
+    except OSError as err:
+        return print(err)
             
 #Copia o apo origem para todos os diretórios de Apo destino em sua pasta de data e hora
-def copiaAPOparaDest(aDirDataHoraDestApos):
-    origem = f'{dirOrigApo}\\{arqApo}'
-    for d in tqdm(aDirDataHoraDestApos):
-        print(f'Copiando de {origem} para {d}.')
+def copiaAPOparaDest(dirDataHoraDestApos):
+    for a in aArqApo:
+        origem = f'{dirOrigApo}\\{a}'
+        print(f'Copiando de {origem} para {dirDataHoraDestApos}.')
         try:
-            shutil.copy(origem, f'{d}\\{arqApo}')  
+            shutil.copy(origem, f'{dirDataHoraDestApos}\\{a}')  
         except OSError as err:
             return print(err)      
 
 #Altera a linha SourcePath de cada um dos INIs para seu respectivo diretório de APO
-####
-
+def substCaminhoRpo(aInis,dirDataHoraDestApos,arqIni):
+    aArquivo=[]
+    appserverini=[]
+    for d in aInis:
+        ini = f'{d}\\{arqIni}'
+        try: 
+            print(ini)
+            with open(ini,'r+') as arquivo:
+                appserverini = ConfigParser.ConfigParser()
+                appserverini.read(ini)
+                for key in aChave:
+                    for section in appserverini.sections():
+                        if appserverini.has_option(section,key):
+                            try: 
+                                if 'custom' in key:
+                                    for c in aArqApo:
+                                        if 'custom' in c: 
+                                            appserverini.set(section,key,f'{dirDataHoraDestApos}\{c}')
+                                else: 
+                                    appserverini.set(section,key,dirDataHoraDestApos)
+                            except OSError as err:
+                                print(err)
+                appserverini.write(arquivo)
+                """ gravaIni = open(ini, 'w')
+                config.write(gravaIni)
+                gravaIni.close() """
+        except OSError as err:
+            print(err)
 try: 
     aInis=leInis(dirInis)
     aDirApos=leDirApos(dirDestApo)
-    aDirDataHoraDestApos=geraDirDataHoraDestAPOs(aDirApos)
-    criaDirDataHoraDestAPOs(aDirDataHoraDestApos)
-    copiaAPOparaDest(aDirDataHoraDestApos)
+    dirDataHoraDestApos=geraDirDataHoraDestAPOs(aDirApos)
+    criaDirDataHoraDestAPOs(dirDataHoraDestApos)
+    copiaAPOparaDest(dirDataHoraDestApos)
+    substCaminhoRpo(aInis, dirDataHoraDestApos, arqIni)
 except OSError as err:
     print(err)
